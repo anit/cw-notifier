3585fa03b015cbf46cb4450dd456fa275ab96a6f
anit <anitrai011@gmail.com>
2021-05-01 16:37:53 +0530

    improve code to read otp optimally

--- App.js
+++ App.js
@@ -20,38 +20,43 @@ import {
 
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
+  Header
 } from 'react-native/Libraries/NewAppScreen';
+import { readOtpFromSms } from './apis';
 
-import SmsAndroid from 'react-native-get-sms-android';
-import CryptoJS from "react-native-crypto-js";
-import { generateOTP } from './apis';
+const App: () => Node = () => {
+  
+  React.useEffect(() => {
+    readOtpFromSms().then(x => console.log('OTP is ', x));
+  }, [])
 
-
-var smsFilter = {
-  minDate: Date.now() - (1000 * 60 * 120), // change last number to change mins.
-  maxDate: Date.now,
-  bodyRegex: '(.*)Your OTP to register\/access CoWIN(.*)', // content regex to match
-  maxCount: 10
+  return (
+    <SafeAreaView style={Colors.darker}>
+      <StatusBar barStyle={'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={Colors.darker}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: Colors.black,
+          }}>
+          <Section title="Logs">
+          </Section>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
-const getSecret = () => {
-  const key = 'CoWIN@$#&*(!@%^&';
-  return CryptoJS.AES.encrypt('b5cab167-7977-4df1-8027-a63aa144f04e', key).toString();
-}
-
 const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
       <Text
         style={[
           styles.sectionTitle,
           {
-            color: isDarkMode ? Colors.white : Colors.black,
+            color: Colors.white,
           },
         ]}>
         {title}
@@ -60,64 +65,12 @@ const Section = ({children, title}): Node => {
         style={[
           styles.sectionDescription,
           {
-            color: isDarkMode ? Colors.light : Colors.dark,
+            color: Colors.light,
           },
         ]}>
         {children}
       </Text>
     </View>
-  );
-};
-
-const App: () => Node = () => {
-  const [smsList, setSmsList] = React.useState([]);
-  const [secret, ] = React.useState(getSecret())
-  const regex = /OTP .{0,} CoWIN is (.*)\. It will be valid/i;
-  SmsAndroid.list(
-    JSON.stringify(smsFilter),
-    (fail) => {
-      console.log('Failed with this error: ' + fail);
-    },
-    (count, response) => {
-      setSmsList(JSON.parse(response));
-    },
-  );
-
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-
-  React.useEffect(() => {
-    console.log('Making api call....')
-    generateOTP(getSecret(), '9665549658').then(x => {
-      console.log('response is ', x);
-    }, e => console.log('error is ', e));
-    
-  }, [])
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Logs">
-            OTP for now is: {
-              smsList.map(x => regex.exec(x.body)[1])
-            }
-
-            Secret is {secret}
-          </Section>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
   );
 };
 
--- apis.js
+++ apis.js
@@ -1,10 +1,72 @@
-export const generateOTP = (secret, mobile) => {
-  return fetch('https://cdn-api.co-vin.in/api/v2/auth/generateMobileOTP', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ mobile, secret })
-  });
+import SmsAndroid from 'react-native-get-sms-android';
+import CryptoJS from "react-native-crypto-js";
+
+export const getSecret = () => {
+  const key = 'CoWIN@$#&*(!@%^&';
+  const userId = 'b5cab167-7977-4df1-8027-a63aa144f04e';
+
+  return CryptoJS.AES.encrypt(userId, key).toString();
+}
+
+export const requestOTP = (mobile) => {
+  const secret = getSecret();
+  return new Promise((resolve, reject) => {
+    fetch('https://cdn-api.co-vin.in/api/v2/auth/generateMobileOTP', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ mobile, secret })
+    })
+    .then(response => response.json())
+    .then(json => {
+      if (json && json.txnId) resolve(json.txnId);
+      else reject();
+    })
+    .catch(e => reject(e));
+  }) 
+}
+
+export const readOtpFromSms = () => {
+  const regex = /OTP .{0,} CoWIN is (.*)\. It will be valid/i;
+  var retries = 0;
+  var maxRetry = 300;
+
+  return new Promise((resolve, reject) => { 
+    var interval = setInterval(() => {
+      retries++;
+      SmsAndroid.list(
+        JSON.stringify({
+          minDate: Date.now() - (1000 * 60 * 20), // change last number to change mins.
+          maxDate: Date.now,
+          bodyRegex: '(.*)Your OTP to register\/access CoWIN(.*)', // content regex to match
+          maxCount: 1
+        }),
+        (fail) => { manageRetry(); console.log('sms read fail: ' + fail); },
+        (count, resp) => {
+          let sms = JSON.parse(resp);
+          if (!sms[0] || !sms[0].body) return;
+  
+          const otp = regex.exec(sms[0].body)[1];
+
+          // Otp found. Moving ahead.
+          if (otp) {
+            clearInterval(interval);
+            resolve(otp);
+          }
+
+          manageRetry()
+        },
+      );
+    }, 2000);
+
+    const manageRetry = () => {
+      // Reject after a certain retries. We are not gonna get anything  now.
+      if (retries >= maxRetry) {
+        clearInterval(interval);
+        reject();
+      }
+    }
+  })
 }
